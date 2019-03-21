9e887430b4bd3e306eff76bd82bdf39fa976de8f
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-03-21 15:53:47 -0600

    [ADD] project to solution

--- ordenamiento/Burbuja.cs
+++ ordenamiento/Burbuja.cs
@@ -6,6 +6,9 @@ namespace ordenamiento
 {
     public class Burbuja
     {
-        
+        public void Ordenar(int[] arr) 
+        {
+            throw new NotImplementedException("Crear los casos de prueba");
+        }
     }
 }
